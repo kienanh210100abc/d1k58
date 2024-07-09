da1c20b9d577fafb890caf142a96a5f0b2aae287
kienanh210100abc <duckien210100@gmail.com>
2024-07-09 11:01:36 +0700

    change metadata E

--- src/ui-component/extended/AppBar.tsx
+++ src/ui-component/extended/AppBar.tsx
@@ -32,6 +32,7 @@ import menuTXL from 'assets/images/landing/Home/MenuTXL.svg';
 import { useDispatch } from 'react-redux';
 import { setLanguage } from 'redux/slice/app.slice';
 import useConfig from 'hooks/useConfig';
+import { useNavigate } from 'react-router-dom';
 
 interface ElevationScrollProps {
     children: ReactElement;
@@ -104,6 +105,7 @@ const AppBar = ({ ...others }) => {
     // };
     const [currentLanguage, setCurrentLanguage] = useState('VN');
     // const [messages, setMessages] = useState(getMessages('VN'));
+    const navigate = useNavigate();
 
     const handleClose = (language: 'VN' | 'EN') => {
         onChangeLocale(language);
@@ -113,6 +115,17 @@ const AppBar = ({ ...others }) => {
         // setMessages(getMessages(language));
         localStorage.setItem('selectedLanguage', language);
         setAnchorEl(null);
+        if (language === 'EN') {
+            // Chuyển sang tiếng Anh
+            if (!location.pathname.startsWith('/en')) {
+                navigate('/en' + location.pathname);
+            }
+        } else {
+            // Chuyển sang tiếng Việt
+            if (location.pathname.startsWith('/en')) {
+                navigate(location.pathname.replace('/en', ''));
+            }
+        }
     };
     useEffect(() => {
         const savedLanguage = localStorage.getItem('selectedLanguage');
