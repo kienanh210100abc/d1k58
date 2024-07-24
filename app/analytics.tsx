e174e56653cd91edee7482f25a8302bea0ba2fde
kienanh210100abc <duckien210100@gmail.com>
2024-07-24 11:40:53 +0700

    banner english meta data

--- app/analytics.tsx
+++ app/analytics.tsx
@@ -1,16 +1,16 @@
-'use client';
+"use client";
 
-import Script from 'next/script';
+import Script from "next/script";
 
 export function Analytics() {
   return (
     <>
       <noscript>
         <iframe
-          src="https://www.googletagmanager.com/ns.html?id=GTM-P5KK4QV"
+          src="https://www.googletagmanager.com/ns.html?id=GTM-KKJK64VK"
           height="0"
           width="0"
-          style={{ display: 'none', visibility: 'hidden' }}
+          style={{ display: "none", visibility: "hidden" }}
         />
       </noscript>
       <Script
@@ -21,7 +21,7 @@ export function Analytics() {
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-P5KK4QV');`,
+        })(window,document,'script','dataLayer','GTM-KKJK64VK');`,
         }}
       />
     </>
